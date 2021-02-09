316ef55c5650c705b4da45641091b60b1c929b14
Max Wubbenhorst <max.wubbenhorst@gmail.com>
2021-02-09 20:25:26 +0100

    check querry

--- Queries.py
+++ Queries.py
@@ -63,4 +63,35 @@ def getQuery02():
     """
     return query
 
+def getQuery03():
+    query = """
+SELECT 
+    opl_ID,
+    category,
+    SUM(nb_failed != 0) AS Failed_Submissions,
+    SUM(nb_failed = 0) AS Successfulll_Submissions,
+    SUM(r.style_result > '') AS amount_bad_style_submissions,
+    SUM(s.points_awarded) AS Points,
+    SUM(a.deadline * 10000 > s.timestamp) AS On_Time,
+    SUM(a.deadline * 10000 < s.timestamp) AS Too_Late,
+    score_prolog,
+    score_haskell,
+    u.user_id
+FROM
+    submissions AS s
+        INNER JOIN
+    results AS r ON r.submission_id = s.submission_id
+        INNER JOIN
+    assignments AS a ON a.assignment_id = s.assignment_id
+        INNER JOIN
+    users AS u ON u.user_id = s.user_id
+        INNER JOIN
+    grades AS g ON g.user_id = u.user_id
+        INNER JOIN
+    education_type AS e ON e.KULopl = u.KULopl
+GROUP BY u.user_id , category
+ORDER BY a.category ASC
+    """
+    return query
+
 
